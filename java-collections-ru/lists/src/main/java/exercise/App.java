9e9e7b22b8c389c80ebb57eb11ad0d958d557de3
Barlog7 <barlog7@yandex.ru>
2023-10-20 15:52:49 +0300

    make scramble logic

--- java-collections-ru/lists/src/main/java/exercise/App.java
+++ java-collections-ru/lists/src/main/java/exercise/App.java
@@ -7,10 +7,37 @@ import java.util.List;
 // BEGIN
 public class App {
     public static void main(String[] args) {
+        App.scrabble("rkqodlw", "world"); // true
+        App.scrabble("ajv", "java"); // false
+        App.scrabble("avjafff", "JaVa"); // true
+        App.scrabble("", "hexlet"); // false
+
 
     }
     public static boolean scrabble(String letters, String word) {
-        List<String> list = new ArrayList<>();
+        if (word.length() > letters.length()) {
+            return false;
+        }
+        String[] strSplitLetters = letters.split("");
+        //List<String> listLetters = Arrays.asList(strSplitLetters);
+        List<String> listLetters = new ArrayList<String>(Arrays.asList(strSplitLetters));
+        String[] strSplitWord = word.split("");
+        //List<String> listWord = Arrays.asList(strSplitWord);
+        List<String> listWord = new ArrayList<String>(Arrays.asList(strSplitWord));
+
+
+        return isBildWord(listLetters, listWord);
+    }
+    public static boolean isBildWord(List<String> listLetters, List<String> listWord) {
+        for (int i = 0; i < listWord.size(); i++) {
+            String leterWord = listWord.get(i).toLowerCase();
+            boolean find = listLetters.contains(leterWord);
+            if (!find) {
+                return false;
+            }
+            listLetters.remove(leterWord);
+
+        }
         return true;
     }
 }
