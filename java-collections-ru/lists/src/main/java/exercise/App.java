3241f1435a975f31e6e8b09fd54dd798da2a0583
Barlog7 <barlog7@yandex.ru>
2023-10-20 12:40:10 +0300

    add class and metod

---
+++ java-collections-ru/lists/src/main/java/exercise/App.java
@@ -0,0 +1,17 @@
+package exercise;
+
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
+
+// BEGIN
+public class App {
+    public static void main(String[] args) {
+
+    }
+    public static boolean scrabble(String letters, String word) {
+        List<String> list = new ArrayList<>();
+        return true;
+    }
+}
+//END
