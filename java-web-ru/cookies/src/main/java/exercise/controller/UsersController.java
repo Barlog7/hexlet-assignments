aaa39f4e31e600aff4164f4fa84ff08afb5da746
@hexlet/cli <support@hexlet.io>
2024-06-13 12:20:54 +0300

    submit java-web-ru/cookies

---
+++ java-web-ru/cookies/src/main/java/exercise/controller/UsersController.java
@@ -0,0 +1,51 @@
+package exercise.controller;
+
+import org.apache.commons.lang3.StringUtils;
+import exercise.util.Security;
+import exercise.model.User;
+import exercise.util.NamedRoutes;
+import static io.javalin.rendering.template.TemplateUtil.model;
+import exercise.repository.UserRepository;
+import exercise.dto.users.UserPage;
+import io.javalin.http.NotFoundResponse;
+import io.javalin.http.Context;
+
+
+public class UsersController {
+
+    public static void build(Context ctx) throws Exception {
+        ctx.render("users/build.jte");
+    }
+
+    // BEGIN
+    public static void create(Context ctx) {
+        var firstName = ctx.formParam("firstName");
+        var lastName = ctx.formParam("lastName");
+        var email = ctx.formParam("email");
+        var password = ctx.formParam("password");
+        var token = Security.generateToken();
+
+        ctx.cookie("token", token);
+
+        var user = new User(firstName, lastName, email, password, token);
+        //var course = new Course(name, description);
+
+        UserRepository.save(user);
+        var id = user.getId();
+        ctx.redirect(NamedRoutes.userPath(id));
+    }
+
+    public static void show(Context ctx) {
+        var id = ctx.pathParamAsClass("id", Long.class).get();
+        var user = UserRepository.find(id)
+                .orElseThrow(() -> new NotFoundResponse("Entity with id = " + id + " not found"));
+        var page = new UserPage(user);
+        var token = ctx.cookie("token");
+        if (token.equals(user.getToken())) {
+            ctx.render("users/show.jte", model("page", page));
+        } else {
+            ctx.render("/users/build");
+        }
+    }
+    // END
+}
